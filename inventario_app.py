9f4767ae0e23928cb962a8ba29a51822e63ac287
Yoinner <yoinergandara28@gmail.com>
2024-06-12 20:20:20 -0500

    Update inventario_app.py

--- inventario_app.py
+++ inventario_app.py
@@ -2,13 +2,12 @@ import streamlit as st
 import pandas as pd
 
 # Enlace de Google Sheets
-google_sheets_url = 'https://docs.google.com/spreadsheets/d/1Y0MQArGYk4k0UD1PQwIvcbgGdpSLZQGP/edit?usp=sharing'
+google_sheets_url = 'https://docs.google.com/spreadsheets/d/1Y0MQArGYk4k0UD1PQwIvcbgGdpSLZQGP/edit?usp=sharing&ouid=104131502310043866764&rtpof=true&sd=true'
 
 # Convertir el enlace de Google Sheets a un enlace exportable en formato CSV
 csv_export_url = google_sheets_url.replace('/edit?usp=sharing', '/gviz/tq?tqx=out:csv')
 
-# Función para cargar datos desde Google Sheets
-@st.cache
+# Función para cargar datos desde Google Sheets sin usar cache
 def load_data(url):
     return pd.read_csv(url)
 
@@ -26,13 +25,19 @@ st.markdown(
     """
     <style>
     .main {
-        background-color: #f5f5f5;
-        background-image: url('https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini-dark.png');
-        color: black;
+        background-image: url('https://raw.githubusercontent.com/Yoinner/streamlit_app.py/main/abstract-grey-background-poster-with-dynamic-design-vector.jpg');
+        background-size: cover;
+        color: white;
     }
     .stTextInput > div > div > input {
-        background-color: #f0f0f0;
+        background-color: rgba(255, 255, 255, 0.8);
         color: black;
+    }
+    .stMarkdown {
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 10px;
+        border-radius: 10px;
+        color: white;
     }
     </style>
     """,
@@ -47,11 +52,13 @@ if query:
     resultados = buscar_productos(query)
     if not resultados.empty:
         for idx, row in resultados.iterrows():
-            st.write(f"**Nombre del Producto:** {row['Nombre del Producto']}")
-            st.write(f"**Cantidad Disponible:** {row['Stock Actual']}")
-            st.write(f"**Precio de Compra:** {row['Precio de Compra']}")
-            st.write(f"**Precio de Venta:** {row['Precio de Venta']}")
-            st.write(f"**Ubicación:** {row['Ubicación']}")
-            st.write("---")
+            st.markdown(f"""
+                **Nombre del Producto:** {row['Nombre del Producto']}  
+                **Cantidad Disponible:** {row['Stock Actual']}  
+                **Precio de Compra:** {row['Precio de Compra']}  
+                **Precio de Venta:** {row['Precio de Venta']}  
+                **Ubicación:** {row['Ubicación']}  
+                ---
+            """)
     else:
         st.write("No se encontraron productos.")
